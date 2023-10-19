2a81a60c892913b76b753252c544e7c58d55bb52
Guillermo849 <orivealonsoguillermo@gmail.com>
2023-10-19 20:54:03 +0200

    FIXED Cerrar Ventana primary stage

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 	
+import javafx.event.EventHandler;
+
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.fxml.FXMLLoader;
 
@@ -13,8 +16,18 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			StackPane root = FXMLLoader.load(getClass().getResource("/fxml/reloj.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,460,400);
+			primaryStage.setForceIntegerRenderScale(false);
 			primaryStage.setScene(scene);
+			/* Cuando se cierra la primary stage la applicación dejará de funcionar */
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+	            @Override
+	            public void handle(WindowEvent t) {
+	                Platform.exit();
+	                System.exit(0);
+	            }
+	        });
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
