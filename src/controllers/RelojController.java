b34b3f32ff1dfce127c3497e5460514de9ffa32c
Guillermo849 <orivealonsoguillermo@gmail.com>
2023-10-19 08:47:24 +0200

    ADD Reloj Controlador

---
+++ src/controllers/RelojController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+public class RelojController implements Initializable {
+
+    @FXML
+    private ImageView imgHDec;
+
+    @FXML
+    private ImageView imgHCen;
+
+    @FXML
+    private ImageView imgMinDec;
+
+    @FXML
+    private ImageView imgMinCen;
+
+    @FXML
+    private ImageView imgSegDec;
+
+    @FXML
+    private ImageView imgSegCen;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
